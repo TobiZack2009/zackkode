3bab8f9054ef7265b81fab504b6d60dd7ca9c306
Tobi Zack <zacktobi2009@outlook.com>
2022-12-16 11:28:04 +0100

    c

--- pong/script.js
+++ pong/script.js
@@ -1,22 +1,37 @@
-var canvas = document.getElementById("c");
+let canvas = document.getElementById("c");
 ctx = canvas.getContext("2d");
-scaleX = innerWidth / canvas.width;
-scaleY = innerHeight / canvas.height;
+scaleX = Math.floor(innerWidth / canvas.width);
+scaleY = Math.floor(innerHeight / canvas.height);
 scaleToFit = Math.min(scaleX, scaleY);
 scaleToCover = Math.max(scaleX, scaleY);
 const degToRad = Math.PI / 180;
 
-function collidedWith(object){
-   rect1=this; rect2=object
-    return rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y < rect2.y + rect2.height &&
-   rect1.height + rect1.y > rect2.y
+function touching(object) {
+  rect1 = this;
+  rect2 = object;
+  if (rect1.radius) {
+    rect1.width = rect1.radius;
+    rect1.height = rect1.radius;
+  }
+  if (rect2.radius) {
+    rect2.width = rect2.radius;
+    rect2.height = rect2.radius;
+  }
+
+  return (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.height + rect1.y > rect2.y
+  );
 }
 
-//canvas.style.transformOrigin = '0 0';
-//scale from top left
-//canvas.style.transform = `scale(${(scaleToFit)})`;
+scaleX = window.innerWidth / canvas.width;
+scaleY = window.innerHeight / canvas.height;
+scaleToFit = Math.floor(Math.min(scaleX, scaleY));
+scaleToCover = Math.floor(Math.max(scaleX, scaleY));
+canvas.style.transformOrigin = "";
+canvas.style.transform = `scale(${scaleToFit})`;
 
 world = {};
 world.width = 720;
@@ -40,8 +55,9 @@ rect = function (x, y, w, h, color = "rgb(0 0 0)") {
   this.xVel = 0;
   this.yVel = 0;
   this.angle = 0;
-  this.collidedWith=collidedWith
-  this.bound=true
+  this.touching = touching;
+  this.bound = true;
+  this.angle = 0;
   drawQueue.push(this);
   return this;
 };
@@ -54,9 +70,10 @@ circle = function (x, y, r, color = "rgb(0 0 0)") {
   this.color = color;
   this.xVel = 0;
   this.yVel = 0;
-  this.bound=true
+  this.bound = true;
+  this.angle = 0;
 
-  this.collidedWith=collidedWith
+  this.touching = touching;
   drawQueue.push(this);
   return this;
 };
@@ -72,8 +89,9 @@ sprite = function (x, y, img, w, h) {
   this.angle = 0;
   this.xVel = 0;
   this.yVel = 0;
-  this.bound=true
-  this.collidedWith=collidedWith
+  this.bound = true;
+  this.touching = touching;
+  this.angle = 0;
   drawQueue.push(this);
   return this;
 };
@@ -81,27 +99,28 @@ sprite = function (x, y, img, w, h) {
 drawQueue = [];
 
 main = function () {
- // console.timeEnd("dt");
+  // console.timeEnd("dt");
   var fillStyle = ctx.fillStyle;
   ctx.fillStyle = "white";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = fillStyle;
   //Clear the canvas before redrawing
-  for (i in drawQueue) {
+  for (let i = 0; i < drawQueue.length; i++) {
     d = drawQueue[i];
     d.x += d.xVel;
     d.y += d.yVel;
-    if(d.bound){
-      if(d.x<=0||d.y<=0){
-        d.x=d.x
-        d.y=d.y
-     }
-     if(this.x>=360||this.y>=240){
-       d.x=d.x;
-       d.y=d.y;
-     }
+    if (d.bound) {
+      if (d.x <= 0 || d.y <= 0) {
+        d.x = d.x;
+        d.y = d.y;
+      }
+      if (this.x >= 360 || this.y >= 240) {
+        d.x = d.x;
+        d.y = d.y;
+      }
     }
-    ctx.rotate(d.angle * degToRad);
+    ctx.rotate((Math.PI / 180) * d.angle);
+    ctx.rotate(0);
     if (d.type == "rect") {
       ctx.fillStyle = d.color;
       ctx.fillRect(d.x, d.y, d.width, d.height);
@@ -125,12 +144,10 @@ requestAnimationFrame(main);
 addEventListener("resize", function () {
   scaleX = window.innerWidth / canvas.width;
   scaleY = window.innerHeight / canvas.height;
-
-  scaleToFit = Math.min(scaleX, scaleY);
-  scaleToCover = Math.max(scaleX, scaleY);
-  //   canvas.style.transformOrigin = '0 0';
-  //scale from top left
-  // canvas.style.transform = `scale(${(scaleToFit)})`;
+  scaleToFit = Math.floor(Math.min(scaleX, scaleY));
+  scaleToCover = Math.floor(Math.max(scaleX, scaleY));
+  canvas.style.transformOrigin = "";
+  canvas.style.transform = `scale(${scaleToFit})`;
 });
 //Define all events and variables nessecary for them to work.
 
